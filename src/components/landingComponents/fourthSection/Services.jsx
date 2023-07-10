1eb2b078920d4e115baf434c3ed70b30e1372a1c
alejandro-dominguez <alejandro.dominguez1@live.com>
2023-07-10 11:49:02 -0300

    working on sercives section tabs content

--- src/components/landingComponents/fourthSection/Services.jsx
+++ src/components/landingComponents/fourthSection/Services.jsx
@@ -18,31 +18,44 @@ const Services = ({
 
     return (
         <section className='pt-[4.6rem] bg-[#0a0c20]'>
-            <div className='h-screen flex flex-col items-center justify-start bg-[#070714] text-white px-16 z-0'>
+            <div className='h-screen flex flex-col items-center justify-start bg-[#070714] text-white px-16'>
                 <h2 className='services-h2'>
                     soluciones a tu medida
                 </h2>
                 <div className='services-tabs-container'>
                     <ul className={
-                        tabI === 1 ? 'services-tabs-ul left'
-                        : tabI === 2 ? 'services-tabs-ul center'
-                        : tabI === 3 ? 'services-tabs-ul right'
-                        : null}
+                            tabI === 1 ? 'services-tabs-ul left'
+                            : tabI === 2 ? 'services-tabs-ul center'
+                            : tabI === 3 ? 'services-tabs-ul right'
+                            : null
+                        }
                     >
                         <button
                             id='services-tabs-1' 
                             type='button'
-                            className='services-tabs-li-btn'
+                            tabIndex='0'
+                            className={
+                                tabI === 1 ? 'services-tabs-li-btn'
+                                : tabI === 2 ? 'services-tabs-li-btn active'
+                                : tabI === 3 ? 'services-tabs-li-btn active'
+                                : null
+                            }
                             onClick={(e) => setTabs(e)}
                         >
-                            <li className={'services-tabs-li'}>
+                            <li className='services-tabs-li'>
                                 Nuestros vehículos
                             </li>
                         </button>
                         <button
                             id='services-tabs-2'
                             type='button'
-                            className='services-tabs-li-btn center'
+                            tabIndex='0'
+                            className={
+                                tabI === 1 ? 'services-tabs-li-btn active'
+                                : tabI === 2 ? 'services-tabs-li-btn'
+                                : tabI === 3 ? 'services-tabs-li-btn active'
+                                : null
+                            }
                             onClick={(e) => setTabs(e)}
                         >
                             <li className='services-tabs-li'>
@@ -52,7 +65,13 @@ const Services = ({
                         <button
                             id='services-tabs-3'
                             type='button'
-                            className='services-tabs-li-btn hover'
+                            tabIndex='0'
+                            className={
+                                tabI === 1 ? 'services-tabs-li-btn active'
+                                : tabI === 2 ? 'services-tabs-li-btn active'
+                                : tabI === 3 ? 'services-tabs-li-btn'
+                                : null
+                            }
                             onClick={(e) => setTabs(e)}
                         >
                             <li className='services-tabs-li'>
@@ -60,6 +79,11 @@ const Services = ({
                             </li>
                         </button>
                     </ul>
+                    <div className="grid justify-between items-center grid-cols-2">
+                        <div className="grid place-content-start">
+                            
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
